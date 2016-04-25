52174619e4bdd859d4a0561a88c536be8c55d38d
CAOLEI <183515951@qq.com>
2016-04-25 19:02:22 +0800

    log4j

--- src/main/java/cn/rest/controller/GreetingController.java
+++ src/main/java/cn/rest/controller/GreetingController.java
@@ -3,20 +3,24 @@ package cn.rest.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
+@RestController
 public class GreetingController {
-    
-    @RequestMapping(value="/hello.do",method=RequestMethod.GET)
-    public ResponseEntity<Map<String, Object>> hello(){
+
+    private Logger log = Logger.getLogger(this.getClass());
+
+    @RequestMapping(value = "/hello.do", method = RequestMethod.GET)
+    public ResponseEntity<Map<String, Object>> hello() {
+        log.info("进入");
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("message", "Hello");
-        return new ResponseEntity<Map<String, Object>>(map,HttpStatus.OK);
+        log.info("完成");
+        return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
     }
 }
-
